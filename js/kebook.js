0fcd15feb7fb69766693fbf5818c91d4a2e5eb8c
Heitor Schleder <heitorschleder33@gmail.com>
2022-08-21 15:06:29 -0300

    menu and navbar js

--- js/kebook.js
+++ js/kebook.js
@@ -1,14 +1,31 @@
 var navbar = document.getElementById('navbar-itens')
-var links = navbar.getElementsByClassName('nav-link')
+var links = Array.from(navbar.querySelectorAll('a'))
+var menuToggle = document.getElementById('menu-icon')
+var menuItens = document.getElementById('menu-itens')
+var closeMenu = document.getElementById('close-menu')
+var menuLinks = Array.from(menuItens.querySelectorAll('li'))
 
-for(let i = 0; i < links.length; i++){
-    links[i].addEventListener('click', () => {
+
+links.forEach(navLink => {
+    navLink.addEventListener('click', function() {
         let current = document.getElementsByClassName('active')
         if(current.length){
             current[0].className = current[0].className.replace(' active', '')
         }
-        this.className += ' active'
+        this.classList.add('active')
     })
-}
+});
 
-// addEventListener = escutando evento "click" pra dispara uma ação de
+menuToggle.addEventListener('click', function() {
+    menuItens.classList.toggle('menu-active');
+});
+
+closeMenu.addEventListener('click', function() {
+    menuItens.classList.toggle('menu-active');
+});
+
+menuLinks.forEach(liLinks => {
+    liLinks.addEventListener('click', function() {
+        menuItens.classList.toggle('menu-active');
+    });
+})
